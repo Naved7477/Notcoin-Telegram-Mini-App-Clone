08a9a5f298ad9233969dfd79ac044f5f5f616d0c
Naved7477 <navedsheikh4947@gmail.com>
2026-09-24 19:30:58 +0530

    Buttons updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,7 @@
 import { useEffect, useState } from 'react';
 import './index.css';
 import Arrow from './icons/Arrow';
-import { bear, coin, highVoltage, notcoin, rocket, trophy } from './images';
-
+import { coin, highVoltage, notcoin, trophy } from './images';
 
 const showUnityAd = () => {
   if ((window as any).unityAds) {
